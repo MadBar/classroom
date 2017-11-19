f453052a280e8fbdc04efc2d2c5084b65aff10e4
Rafael Silva <rasilva.1986@gmail.com>
2017-11-19 13:54:21 +0100

    small modification on the main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,11 +7,11 @@ import otherclasses.Student;
 
 public class Main {	
 	public static void main(String[] args) {
-		ArrayList<Student> students = new ArrayList<>();
+		ArrayList<Student> students = new ArrayList<Student>();
 		Classroom classroom = new Classroom("TEST15", "Spring 16", students);
 		Student student1 = new Student("Rafael", "Silva", 20, 'M', 3.1, 5.4, 6.2);
 		Student student2 = new Student("Fredrik", "Mellgren", 20, 'M', 8.0, 8.8, 6.1);
-		Student student3 = new Student("Hans", "Ekström", 20, 'M', 8.8, 5.0, 6.0);
+		Student student3 = new Student("Hans", "Ekstrï¿½m", 20, 'M', 8.8, 5.0, 6.0);
 		Student student4 = new Student("Patrik", "Hollsten", 20, 'F', 8.2, 7.3, 6.0);
 		Student student5 = new Student("Gopi", "Sundarraj", 20, 'M', 8.2, 9.0, 6.0);
 
