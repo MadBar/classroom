fb92ff13fc2c72bbced6b7cd124fb5b23c4b60a7
MadBar <madeleinetrollvik@gmail.com>
2017-11-25 22:22:32 +0100

    Implemented getters for PersonTest
    Question: Can you only test get methods? Because they setters don't have a return? Look into later
    
    Seems to need returnb.. How?

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -6,10 +6,41 @@ import otherclasses.Person;
 
 public class PersonTest {
 
+	// First chois to have all tests of the classes all methods in one case
+
+	// Ska man bara testa med en person? Makes sense I guess. If it works with one
+	// it might work for everybody?
+	// Question: Is there a point to have all created persons here?
+	Person p = new Person("Rafael", "Silva", 30, 'M');
+
 	@Test
-	public void test() {
-		Person p = new Person("Rafael", "Silva", 30,'M');
-		assertEquals(p.getFirstName().equals("Rafael"),true);
+	public void testGetAge() {
+
+		assertEquals(p.getAge(), 30);
+	}
+	
+	@Test
+	public void testGetFirstName() {
+
+		assertEquals(p.getFirstName().equals("Rafael"), true);
 	}
 
+	//Can you only test get methods? Because they setters don't have a return? Look into later
+//	@Test
+//	public void testSetFirstName() {
+//
+//		assertEquals(p.setFirstName().equals("Rafael"), true);
+//	}
+	
+	@Test
+	public void testGetLastName() {
+
+		assertEquals(p.getLastName().equals("Silva"), true);
+	}
+	
+	@Test
+	public void testGetGender() {
+
+		assertEquals(p.getGender(),'M');
+	}
 }
